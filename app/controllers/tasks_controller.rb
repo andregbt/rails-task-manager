a865dcbaf6cd4deee17a71522dbe3a7a83a7c88c
andre teixeira <andre.gomes.teixeira22@gmail.com>
2022-11-22 15:29:52 +0000

    taskmanager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,17 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+    @task.save
+  end
+
+  def create
+  end 
+  # private
+
+  # def task_params
+  #   params.require(:task).permit(:title, :details)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
   get 'tasks/:id', to: 'tasks#show', as: 'task'
+
 end
